11243f3cfded519156084d393ccddb95e29bb4e7
Onlyain <1115236203@qq.com>
2026-06-01 14:07:10 +0800

    完善后端框架

--- src/backend/edu_backend/urls.py
+++ src/backend/edu_backend/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("<h1>🚀 后端框架已启动成功！</h1>")
 
 urlpatterns = [
+    path("", home),  # 根路径欢迎页
     path("admin/", admin.site.urls),
-    path("api/", include("apps.api.urls")),  # 接入 api 应用
+    path("api/", include("apps.api.urls")),
 ]
